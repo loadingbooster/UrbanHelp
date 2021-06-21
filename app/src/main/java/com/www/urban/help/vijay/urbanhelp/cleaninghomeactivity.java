da1b7bc4ec4e8c330eb461c37b411c33bff129eb
Ranjan Sharma <sranjansharam2001@gmail.com>
2021-06-22 01:28:13 +0530

    some changes

--- app/src/main/java/com/www/urban/help/vijay/urbanhelp/cleaninghomeactivity.java
+++ app/src/main/java/com/www/urban/help/vijay/urbanhelp/cleaninghomeactivity.java
@@ -71,7 +71,7 @@ public class cleaninghomeactivity extends AppCompatActivity {
         chipcooking.setOnCloseIconClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int cooking=0;
+                int cooking;
                 if(chipcooking.isChecked()){
                     cooking = 1;
                 }
